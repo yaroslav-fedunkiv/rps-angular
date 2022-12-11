02ae505852c785f4ab3a7c85d2e0ba63f0fe1a00
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-11 22:49:15 +0200

    work with header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
