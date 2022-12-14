a641e2061f0f0e8400007abe429b500731952f95
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-14 09:57:46 +0200

    work with publisher list #5

---
+++ src/app/publishers/full-publisher.model.ts
@@ -0,0 +1,11 @@
+export interface FullPublisherModel{
+  id: string;
+  title: string;
+  topic: string;
+  price: string;
+  image: string;
+  description: string;
+  isActive: string;
+  created: string;
+  updated: string;
+}
--- src/app/publishers/publisher-list/publisher-list.component.ts
+++ src/app/publishers/publisher-list/publisher-list.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Injectable, OnDestroy, OnInit} from '@angular/core';
 import {Publisher} from "../publisher.model";
 import {Subscription} from "rxjs";
 import {PublisherService} from "../publisher.service";
+import {FullPublisherModel} from "../full-publisher.model";
 
 @Component({
   selector: 'app-publisher-list',
   templateUrl: './publisher-list.component.html',
   styleUrls: ['./publisher-list.component.css']
 })
+@Injectable({providedIn: 'root'})
 export class PublisherListComponent implements  OnInit, OnDestroy{
-  publishers: Publisher[];
+  // publishers: Publisher[];
+  publishers: FullPublisherModel[];
   subscription: Subscription;
 
   constructor(private publisherService: PublisherService) {
@@ -17,11 +20,12 @@ export class PublisherListComponent implements  OnInit, OnDestroy{
 
   ngOnInit() {
     this.publisherService.publisherChanged.subscribe(
-      (publishers: Publisher[]) => {
+      // (publishers: Publisher[]) => {
+      (publishers: FullPublisherModel[]) => {
         this.publishers = publishers;
       }
     );
-    this.publishers = this.publisherService.getPublishers();
+    this.publishers = <FullPublisherModel[]><unknown>this.publisherService.getAllPublishers();
   }
 
   ngOnDestroy() {
