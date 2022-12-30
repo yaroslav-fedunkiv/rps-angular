7236b450681659f61fbf487e65364022d32ac48b
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-30 20:33:12 +0200

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {PublishersComponent} from "./publishers/publishers.component";
 import {RegistrationComponent} from "./admin/registration/registration.component";
+import {DashboardComponent} from "./admin/dashboard/dashboard.component";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/publishers', pathMatch: 'full'},
   {path: 'publishers', component: PublishersComponent},
-  {path: 'sign-up', component: RegistrationComponent}
+  {path: 'sign-up', component: RegistrationComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ]
 
 @NgModule({
